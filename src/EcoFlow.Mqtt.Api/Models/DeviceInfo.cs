128e28d0a2739f24cc28d42465652f5a0011a02d
caunt <gpg@caunt.world>
2026-02-26 03:20:27 +0200

    feat(models): ✨ improve device info string output
    Added a custom string representation for device info objects, displaying the title and the last four characters of the serial number for easier identification.

--- src/EcoFlow.Mqtt.Api/Models/DeviceInfo.cs
+++ src/EcoFlow.Mqtt.Api/Models/DeviceInfo.cs
@@ -1,3 +1,28 @@
-﻿namespace EcoFlow.Mqtt.Api.Models;
+﻿using System.Text;
 
-public record DeviceInfo(string SerialNumber, string Title);
+namespace EcoFlow.Mqtt.Api.Models;
+
+public record DeviceInfo(string SerialNumber, string Title)
+{
+    private string? _cachedToString;
+
+    public override string ToString()
+    {
+        lock (this)
+            return _cachedToString ??= BuildString();
+
+        string BuildString()
+        {
+            var stringBuilder = new StringBuilder();
+
+            if (!Title.Contains("EcoFlow", StringComparison.OrdinalIgnoreCase))
+                stringBuilder.Append("EcoFlow ");
+
+            stringBuilder.Append(Title);
+            stringBuilder.Append(" - ");
+            stringBuilder.Append(SerialNumber, SerialNumber.Length - 4, 4);
+
+            return stringBuilder.ToString();
+        }
+    }
+}
